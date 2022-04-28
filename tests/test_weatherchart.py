4754a63ab7c545b33639a33d539b8f4dd76325ef
pub12 <pubudu79@gmail.com>
2022-04-28 15:41:31 +0000

    version 1.0

--- tests/test_weatherchart.py
+++ tests/test_weatherchart.py
@@ -6,17 +6,24 @@ sys.path.insert(0, '../')
 from weatherchart import WeatherChart
 
 class TestPre(unittest.TestCase):
-    def test_get_weather_by_lat_long(self):
+    def test_0010_get_weather_by_lat_long(self):
         wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
 
         print( wchart.get_forecast('22.300910042194783', '114.17070449064359' ) )
 
-    def test_get_image(self):
+    def test_0020_get_image(self):
         wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
 
         print( wchart.get_forecast_chart_image('22.300910042194783', '114.17070449064359', 'out.jpg' ) )
 
+    def test_0030_get_forecast_chart_image_by_loc(self):
+        wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
+        print( wchart.get_forecast_chart_image_by_loc('Tai Wai, Hong Kong', 'out.jpg' ) )
 
+
+    def test_0040_get_forecast_chart_image_by_loc(self):
+        wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
+        print( wchart.get_forecast_chart_image_by_loc('Tokyo, Japan', 'out.jpg' ) )
 
 # if __name__ == '__main__':
 # 	print("Getting Weather Data")
