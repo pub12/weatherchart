2675e3badae5c09b4d2c7107def501d698d3087b
pub12 <pubudu79@gmail.com>
2022-05-02 14:25:54 +0000

    Readme update

--- tests/test_weatherchart.py
+++ tests/test_weatherchart.py
@@ -11,11 +11,6 @@ class TestPre(unittest.TestCase):
 
         print( wchart.get_forecast('22.300910042194783', '114.17070449064359' ) )
 
-    def test_0020_get_image(self):
-        wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
-
-        print( wchart.get_forecast_chart_image('22.300910042194783', '114.17070449064359', 'out.jpg' ) )
-
     def test_0030_get_forecast_chart_image_by_loc(self):
         wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
         print( wchart.get_forecast_chart_image_by_loc('Tai Wai, Hong Kong', 'out.jpg' ) )
@@ -25,6 +20,6 @@ class TestPre(unittest.TestCase):
         wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
         print( wchart.get_forecast_chart_image_by_loc('Tokyo, Japan', 'out.jpg' ) )
 
-# if __name__ == '__main__':
-# 	print("Getting Weather Data")
-# 	print( get_weather_data_by_location( '22.300910042194783', '114.17070449064359') )
+    def test_0050_get_forecast_chart_image_by_loc_no_file(self):
+        wchart = WeatherChart('1f285517a7b88e19c9f4631ff1cd368a', 'Asia/Hong_Kong')
+        print( wchart.get_forecast_chart_image_by_loc('Tokyo, Japan'  ) )
--- weatherchart/weatherchart.py
+++ weatherchart/weatherchart.py
@@ -1,13 +1,19 @@
 #weather_forecast.py
-import requests , pytz, datetime
+import requests , pytz, datetime, math
 from quickchart import QuickChart
+from geopy.geocoders import Nominatim
+
 
 class WeatherChart:
+	#############################################################################################
+	# Constructor
 	def __init__(self, map_key, tz_location='Asia/Singapore' ) :
 		self.apikey = map_key 
 		self.tz = pytz.timezone( tz_location )
 
-	def get_full_weather_data_by_location(self, lat, long):
+	#############################################################################################
+	# Given a lat-long, then get a detailed json of weather data
+	def get_full_weather_data(self, lat, long):
 		url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&appid={self.apikey}&units=metric'
 		r = requests.get(url)
 		return r.json()
@@ -16,15 +22,24 @@ class WeatherChart:
 		else:
 			return None
 
+	#############################################################################################
+	# Given a location string (e.g. "Tokyo, Japan" or just "Seattle") then return weather dict
+	def get_forecast_by_location(self, loc_string):
+		geolocator = Nominatim(user_agent="pubs_01")
+		location = geolocator.geocode( loc_string )
+		return self.get_forecast( location.lat, location.longitude)
+
+	#############################################################################################
+	# Given a lat long then return weather dict
 	def get_forecast(self, lat, long):
-		weather = self.get_full_weather_data_by_location(lat, long)
+		weather = self.get_full_weather_data(lat, long)
 
 		forecast = {}
 		for index in range( 0, 8):
 			local_time = datetime.datetime.fromtimestamp( weather['daily'][index]['dt'] , tz=self.tz )
 			str_time = local_time.strftime( '%a %Y-%m-%d' )
 			forecast[ str_time ] =  {
-										'temp': weather['daily'][index]['temp']['day'],
+										'temp': round(weather['daily'][index]['temp']['day'],1),
 										'temp_min': weather['daily'][index]['temp']['min'],
 										'temp_max': weather['daily'][index]['temp']['max'],
 										'wind_speed': weather['daily'][index]['wind_speed'],
@@ -33,8 +48,9 @@ class WeatherChart:
 
 		return forecast
 
-
-	def get_forecast_chart_image( self, lat, long , output_file):
+	#############################################################################################
+	# Given a location string (e.g. "Tokyo, Japan" or just "Seattle") then return chart image
+	def get_forecast_chart_image_by_loc( self, loc_string,   output_file=None):
 		qc = QuickChart() 
 		qc.width = 500 #set width and height of chart in pixels
 		qc.width = 500
@@ -42,29 +58,35 @@ class WeatherChart:
 		labels = []	#Declare to hold the x-axis tick labels
 		weather_readings = []  #get the data labels
 		
-		forecast = self.get_forecast( lat, long)
+		forecast = self.get_forecast_by_location( loc_string )
 		
 		for item_key in forecast.keys():
 			labels.append( item_key )
 			weather_readings.append( forecast[ item_key ]['temp'] )
 		
-		qc.config = self._get_forecast_chart_image_config( labels, weather_readings)
-		print( qc.get_short_url() ) 	#Print out the chart URL
-		qc.to_file( output_file )	#Save to a file
+		qc.config = self._get_forecast_chart_image_config( loc_string, labels, weather_readings)
+		# print(qc.config)
+		# print( ) 	#Print out the chart URL
+		if output_file: qc.to_file( output_file )	#Save to a file
+		return qc.get_short_url() 
 	
-	def _get_forecast_chart_image_config(self, labels, weather_readings ):
+	#############################################################################################
+	# Internal funcion reutrn the config
+	def _get_forecast_chart_image_config(self, loc_string, labels, weather_readings ):
+		min, max = self._get_forecast_chart_image_config__min_max_tick_item( weather_readings, 2)
+
 		return { 'type': 'line',
-				'data': { 'labels': labels,
+				'data': { 'labels':  [''] + labels +[''],
 				'datasets': [ { 
 								'backgroundColor': 'rgb(255, 99, 132)', 
-								'data':   weather_readings,
+								'data':  [None] + weather_readings ,
 								'lineTension': 0.4,
 								'fill': False,
 								} ],
 						},
 				'options': {
 							'title': { 'display': True,  
-										'text': '7-Day Weather Forecast' }, 
+										'text': '7-Day Weather Forecast: ' + loc_string }, 
 							'legend': { 'display': False}, 
 							'scales': { 'yAxes': [ { 
 														'scaleLabel':  { 
@@ -80,6 +102,35 @@ class WeatherChart:
 															'backgroundColor': '#ccc',
 															'borderRadius': 3
 														},
+										},
+							'scales': {  'yAxes': [{
+											'scaleLabel': {
+											'display': True,
+											'labelString': 'Temperature Degrees Celcius'
+											},
+											'ticks': {
+											'min': min,
+											'max': max,
+											'stepSize': 2  
+											}
+										}]
 										}
-								},
+
+							},
 					} 
+	#############################################################################################
+	# Internal function to setup configuration
+	def _get_forecast_chart_image_config__min_max_tick_item(self, weather_readings, divisor):
+
+		#First find the min and max values
+		min = 1000
+		max = 0
+		for reading in weather_readings:
+			if reading < min: min = reading
+			if reading > max: max = reading
+
+		#Round the values with gaps in between
+		min = math.floor(min * 0.8 / divisor)  * divisor
+		max = math.ceil(max * 1.2 / divisor)  * divisor
+ 
+		return min, max
